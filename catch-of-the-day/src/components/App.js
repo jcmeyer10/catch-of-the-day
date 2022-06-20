b4a3bfab95941e5ed00168a201c0c00425c33862
Jeff Meyer <jcmeyer10@gmail.com>
2022-06-20 15:00:26 -0400

    created props for heaer in App, removed hardcoded tagline from Header component and passed in this.props.tagline

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,11 +8,12 @@ class App extends React.Component {
         return(
             <div className="catch-of-the-day">
                 <div className='menu'>
-                    <Header />
+                    <Header tagline="Fresh Seafood Market"/>
                 </div>
+                
+                <Order />
 
                 <Inventory />
-                <Order />
 
             </div>
         )
