f4ba40046ea4a80b587af44f40677ea37ef7fe1a
Jeff Meyer <jcmeyer10@gmail.com>
2022-06-21 16:46:16 -0400

    adds ref in input and adds store button that console logs store name

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        //stop the form from submitting
+        event.preventDefault;
+
+        // get the text from that input. golden rule is don't touch the DOM
+        // meaning don't go out and f   ind DOM elements for info using selectors
+        // we will sync the data from the form to the 'state'` using refs
+        console.log("myInput value", this.myInput.current.value);
+
+        // change the page to the store / whatever user entered
+
+
+    }
+
     render(){
-        myInput = React.createRef();
-
-        goToStore = (event) => {
-            //stop the form from submitting
-            event.preventDefault;
-
-            // get the text from that input. golden rule is don't touch the DOM
-            // meaning don't go out and find DOM elements for info using selectors
-            // we will sync the data from the form to the 'state'` using refs
-            console.log(this)
-
-            // change the page to the store / whatever user entered
-
-            
-        }
         return (
         <React.Fragment>
         <p>Fish!</p>
         <form className='store-selector' onSubmit={this.goToStore}>
             <h2>Please Enter a Store</h2>
             <input type='text' 
+            ref={this.myInput}
             required 
             placeholder='Store Name' 
             defaultValue={getFunName()}></input>
