82deef51c9117b1fd29776406c33ff3c649e73c1
Jeff Meyer <jcmeyer10@gmail.com>
2022-07-22 14:03:09 -0400

    Working through firebase authentication. not functional yet

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import AddFishForm from './AddFishForm';
-import EditFishForm from './EditFishForm';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import firebase from "firebase";
+import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
 
@@ -12,24 +15,37 @@ class Inventory extends React.Component {
         loadSampleFish: PropTypes.func
     }
 
+    authHandler = async (authData) => {
+        console.log(authData);
+    }
+    
+    authenticate = provider => {
+        const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+        firebaseApp
+          .auth()
+          .signInWithPopup(authProvider)
+          .then(this.authHandler);
+      };
     render(){
-        return (
-            <React.Fragment>
-                <div className='inventory'>
-                    <h2>Inventory</h2>
-                    {Object.keys(this.props.fishes).map(key => <EditFishForm 
-                    key={key}
-                    index={key}
-                    fish={this.props.fishes[key]}
-                    updateFish={this.props.updateFish} 
-                    deleteFish={this.props.deleteFish} />
-                    )}
+        return <Login authenticate={this.authenticate} />;
+        
+        // return (
+        //     <React.Fragment>
+        //         <div className='inventory'>
+        //             <h2>Inventory</h2>
+        //             {Object.keys(this.props.fishes).map(key => <EditFishForm 
+        //             key={key}
+        //             index={key}
+        //             fish={this.props.fishes[key]}
+        //             updateFish={this.props.updateFish} 
+        //             deleteFish={this.props.deleteFish} />
+        //             )}
 
-                    <AddFishForm addFish={this.props.addFish} />
-                    <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
-                </div>
-            </React.Fragment>
-        )
+        //             <AddFishForm addFish={this.props.addFish} />
+        //             <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
+        //         </div>
+        //     </React.Fragment>
+        // )
 }
 }
 
